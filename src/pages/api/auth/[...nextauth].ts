7d797df76cbf6b4a58ab6228bfd648b3f172a215
Breno <bhfs.2908@gmail.com>
2022-03-02 11:02:28 -0300

    feat: finish

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -18,24 +18,37 @@ export default NextAuth({
   ],
   callbacks: {
     async session(session) {
-      const userActiveSubscription = await fauna.query(
-        q.Get(
-          q.Match(
-            q.Index("subscription_by_user_ref"),
-            q.Select(
-              "ref",
-              q.Get(
-                q.Match(
-                  q.Index("user_by_email"),
-                  q.Casefold(session.user.email)
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
                 )
-              )
-            )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
           )
-        )
-      );
+        );
 
-      return session;
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
     },
 
     async signIn({ user, account, profile }) {
